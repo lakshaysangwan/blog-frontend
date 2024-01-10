7bc8354e9018da67d4e8a7b6e6a6981e8eac7a3f
Lakshay Sangwan <lakshay.ib.hisar@gmail.com>
2024-01-10 14:44:55 +0530

    Dark Mode base added.

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module("myApp", ["ngRoute"]);
 
-app.config(['$routeProvider', function ($routeProvider) {
+app.config([
+  "$routeProvider",
+  function ($routeProvider) {
     $routeProvider
-        .when('/', {
-            templateUrl: 'pages/home/home.html',
-            controller: 'HomeController'
-        })
-        .when('/about', {
-            templateUrl: 'pages/about/about.html',
-            controller: 'AboutController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-}]);
+      .when("/", {
+        templateUrl: "pages/home/home.html",
+        controller: "HomeController",
+      })
+      .when("/about", {
+        templateUrl: "pages/about/about.html",
+        controller: "AboutController",
+      })
+      .otherwise({
+        redirectTo: "/",
+      });
+  },
+]);
+
+document
+  .getElementById("dark-mode-toggle")
+  .addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    const icon = this.querySelector("i");
+    if (document.body.classList.contains("dark-mode")) {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    } else {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    }
+  });
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,2 @@
 app.controller('HomeController', function ($scope) {
-    $scope.message = 'Welcome to the home page!';
 });
